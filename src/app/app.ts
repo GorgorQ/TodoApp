98c6ce3535c99a94a1bb365022ce99049524f942
GorgorQ <alexandre.kalaydjian@efrei.net>
2025-07-18 19:31:10 +0200

    starting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
+import { NgForOf, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+
+export interface TodoItem {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgForOf, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'TodoApp';
+  todoList: TodoItem[] = [];
+  newTask: string = '';
+
+  addTask():void {
+    if (this.newTask.trim() !== '') { 
+      const newTodoItem : TodoItem = {
+        id: Date.now(),
+        title: this.newTask,
+        completed: false
+      }
+      this.todoList.push(newTodoItem);
+      console.log('New task added:', this.todoList);
+      this.newTask = '';
+    }
+  }
+  
 }
